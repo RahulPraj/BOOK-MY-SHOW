3cd0b80005123acda41d513ad3bd530c6cda003b
RahulPraj <rahul971801@gmail.com>
2021-07-17 22:47:32 +0530

    add entertainment carousal

--- src/components/HeroCarousal/HeroCarousal.component.jsx
+++ src/components/HeroCarousal/HeroCarousal.component.jsx
@@ -1,21 +1,34 @@
 import React from "react";
 import Slider from "react-slick";
 
+//component
+import { NextArrow, PrevArrow } from "./Arrows.components";
+
 // Import css files
 import "slick-carousel/slick/slick.css";
 import "slick-carousel/slick/slick-theme.css";
 
 const HeroCarousal = () => {
+    const settingsLG = {
+        arrows: true,
+        autoplay:true,
+        centerMode: true,
+        centerPadding:"300px",
+        slidesToShow: 1,
+        infinite: true,
+        slidesToScroll: 1,
+        nextArrow: <NextArrow/>,
+        prevArrow: <PrevArrow/>,
+    };
     const settings = {
         arrows: true,
-        autoplay: true,
-        centerMode: true,
-        centerPadding: "300px",
-        dots: true,
+         dots: true,
         infinite: true,
         speed: 500,
         slidesToShow: 1,
-        slidesToScroll: 1
+        slidesToScroll: 1,
+        nextArrow: <NextArrow/>,
+        prevArrow: <PrevArrow/>,
     };
 
     const images = [
@@ -25,14 +38,26 @@ const HeroCarousal = () => {
         "https://images.unsplash.com/photo-1626050954744-92bf034ce476?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=668&q=80",
         "https://images.unsplash.com/photo-1626074961596-cab914d9392e?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=664&q=80",];    
     return <>
+        <div className="lg:hidden">
         <Slider {...settings}>
             {images.map((image) =>(
-                  <div className="w-20 h-80">
-                      <img src={image} alt="testing" className="w-full h-full" />
+                  <div className="w-full h-56 md:h-80 py-3">
+                      <img src={image} alt="testing" className="w-full h-full " />
                   </div> 
             ))}
 
         </Slider>
+        </div>
+        <div className="hidden lg:block">
+        <Slider {...settingsLG}>
+            {images.map((image) =>(
+                  <div className="w-full h-96 px-2 py-3">
+                      <img src={image} alt="testing" className="w-full h-full rounded-md" />
+                  </div> 
+            ))}
+
+        </Slider>
+        </div>
     </>
 };
 

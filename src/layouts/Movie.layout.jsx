c36fe47ea8a19047f4d2b93e7422dab382ec93a6
RahulPraj <rahul971801@gmail.com>
2021-07-23 00:24:27 +0530

    working on movie info

--- src/layouts/Movie.layout.jsx
+++ src/layouts/Movie.layout.jsx
@@ -1,10 +1,11 @@
 import React from 'react'
+
 //components
-import Navbar from "../components/navbar/navbar.component";
+import MovieNavbar from '../components/navbar/MovieNavbar.component';
 const MovieLayout = (props) => {
     return (
         <>
-            <Navbar />
+            < MovieNavbar  />
            {props.children}  
         </>
     );
--- src/pages/Movie.page.jsx
+++ src/pages/Movie.page.jsx
@@ -1,9 +1,45 @@
 import React from 'react';
 
+import {FaCcVisa,FaCcAmazonPay} from "react-icons/fa";
+
+import MovieHero from '../components/MovieHero/MovieHero.component';
+
 export const Movie= () => {
     return (
         <>
-         hi   
+          <MovieHero/>
+          <div className=" my-12 container  px-4 lg:ml-20 lg:w-2/3">
+            <div className="flex flex-col items-start gap-3">
+            <h2 className="text-gray-800 font-bold text-2xl">About the movie</h2>
+            <p>Bruce Wayne and Diana Prince try to bring the metahumans of Earth together after the death of Clark Kent. Meanwhile, Darkseid sends Steppenwolf to Earth with an army to subjugate humans.</p>
+            </div>
+              <div className ="my-8">
+              <hr />
+              </div>
+              <div className="my-8">
+                  <h1 className="text-gray-800 font-bold text-2xl mb-3">Applicable offers</h1>
+                  <div className="flex flex-col gap-3 lg:flex-row">
+                    <div className="flex items-start gap-2 bg-yellow-100 p-3  border-yellow-400 border-dashed border-2 rounded-md ">
+                      <div className="w-8 h-8">
+                      <FaCcVisa className="w-full h-full"/>
+                      </div>
+                      <div className="flex flex-col items-start">
+                        <h3 className="text-gray-700 text-xl font-bold">Visa Stream Offer</h3>
+                        <p className="text-gray-600">Get 50% off up to INR 150 on all RuPay cards* on BookMyShow Stream.</p>
+                      </div>
+                    </div>
+                    <div className="flex items-start gap-2 bg-yellow-100 p-3  border-yellow-400 border-dashed border-2 rounded-md ">
+                      <div className="w-8 h-8">
+                      <FaCcAmazonPay className="w-full h-full"/>
+                      </div>
+                      <div className="flex flex-col items-start">
+                        <h3 className="text-gray-700 text-xl font-bold">Filmy Pass</h3>
+                        <p className="text-gray-600">Get Rs.75* off on 3 movies you buy/rent on Stream. Buy Filmy Pass @Rs.99</p>
+                      </div>
+                    </div>
+                  </div>
+              </div>
+          </div>
         </>
     )
 }

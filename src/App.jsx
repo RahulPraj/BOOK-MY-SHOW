074b359d9cb63967265094a3ea39c7a287b5df1d
RahulPraj <rahul971801@gmail.com>
2021-07-26 23:23:44 +0530

    add TMDB

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
   
-import { Route } from "react-router-dom";
+import axios from "axios";
 
 // HOC
 import DefaultHOC from "./HOC/Default.HOC";
@@ -15,7 +15,10 @@ import "slick-carousel/slick/slick-theme.css";
 
 
 
-
+//axios default settings
+axios.defaults.baseURL= "https://api.themoviedb.org/3";
+axios.defaults.params = {};
+axios.defaults.params["api_key"] = process.env.REACT_APP_API_KEY;
 
 
 function App() {
